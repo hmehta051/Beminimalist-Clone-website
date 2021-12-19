f83262af6bbf5535a6dd7db48495a86196787903
Sudhanshu894 <sidharts894@gmail.com>
2021-12-19 19:41:57 +0530

    adding all pages

---
+++ components/data.js
@@ -0,0 +1,170 @@
+
+function kdata(){
+
+
+
+const dataarr = [
+
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_d4dbda77-2adc-4d4b-8c29-fd168cdbc2ae_350x300_crop_center.jpg?v=1616389018",
+        title:"Salicylic Acid is One Of The Most Commonly Used Topical Face Acids! Heres How To Use It!",
+        category:"Acne"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_5ed4fc56-3ddd-4a22-8c85-39e2be166515_350x300_crop_center.jpg?v=1616216743",
+        title:"10 Things you need to know about skin hydration",
+        category:"Anti-aging"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_9c219657-ed35-4fcb-8413-8804e32561c9_350x300_crop_center.jpg?v=1616129562 ",
+        title:"The Uses and Benefits of Oleic Acid for Skin ",
+        category:"Anti-inflammatory"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/blog_creative_017cedf8-11c3-445d-9f03-0505c3cbbab3_350x300_crop_center.jpg?v=1616045048",
+        title:"How to maintain pH skin level? Why is it important?",
+        category:"Anti-Oxident"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_c3d42681-b1dc-4b11-bf11-e443aa504096_350x300_crop_center.jpg?v=1615960891",
+        title:" Dry Patches on the Face: Causes & Prevention",
+        category:"Coloidal-Oatmeal"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_d687a031-6f8d-4aa2-a681-b0ad7e5f2da7_350x300_crop_center.jpg?v=1615874050 ",
+        title:" 7 Benefits of Rice Water For Skin ",
+        category:"Hydration"
+
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_9b97690b-b968-438f-88a2-fd554855bbc0_350x300_crop_center.jpg?v=1615809438",
+        title:"Are you worried about dark patches on lips and around the mouth? ",
+        category:"Inflamation"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_1d8c1337-9d5d-4950-8995-4d01e9100158_350x300_crop_center.jpg?v=1615610542 ",
+        title:" Why Does Pigmentation Appear on the Face?  ",
+        category:"Moisturizer"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_df9cbc41-fe84-4c35-ac51-81ab41a39482_350x300_crop_center.jpg?v=1615524094 ",
+        title:" Body Scrub Vs. Exfoliation: What's the difference? ",
+        category:"Peptides"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_9ebd5e54-bc98-495c-b2d8-e28781d5e9da_350x300_crop_center.jpg?v=1615358338 ",
+        title:" How is Tranexamic Acid Special for Skin?        ",
+        category:"Moisturizer"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_2bbd078e-dd02-4111-a007-62c6e63906c5_350x300_crop_center.jpg?v=1615295931        ",
+        title:"Can Sunscreen Prevent Tanning and Premature Skin Aging? Know How Sunscreen Works        ",
+        category:"Peptides"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/essential-5963208_640_350x300_crop_center.jpg?v=1615188418        ",
+        title:" A Concise Guide on Layering With Alpha Arbutin Serum: The Do’s and Don’ts        ",
+        category:"Inflamation"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_dbb7bc32-79c0-4b07-bfae-856b273bea91_350x300_crop_center.jpg?v=1615017205        ",
+        title:"Why is Cold-Pressed Skincare Trending?        ",
+        category:"Hydration"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_4_3d22e89a-d479-4fda-9bb7-4c7944649943_350x300_crop_center.jpg?v=1615016737        ",
+        title:" The Top 11 Skincare Combinations That You Will Absolutely Love!        ",
+        category:"Coloidal-Oatmeal"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_531d8116-fcad-421c-942a-9f412a5c1abb_350x300_crop_center.jpg?v=1614926315        ",
+        title:"Skincare Ingredients Trending in 2021        ",
+        category:"Anti-inflammatory"
+
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_4_f4ecf546-05a6-4a85-b330-2528555fed60_350x300_crop_center.jpg?v=1614925659        ",
+        title:" 9 Mistakes You Can Easily Avoid In Your Skincare Routine!        ",
+        category:"Anti-aging"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_5ad88017-bb8b-40b8-a075-30ca658a59bc_350x300_crop_center.jpg?v=1614843765        ",
+        title:"Choose Face acids Based on Your Skin Type        ",
+        category:"Anti-Oxident"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_25ca5437-702e-4f25-92a4-01a5685044d0_350x300_crop_center.jpg?v=1614838360        ",
+        title:"Skincare Combinations That You Should Avoid        ",
+        category:"Coloidal-Oatmeal"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_4ebaaa14-328a-4177-9e00-2c9804c76038_350x300_crop_center.jpg?v=1614750522        ",
+        title:" The 10 Best Acids for Your Skincare Routine        ",
+        category:"Acne"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_86caeead-8f10-4f6b-bf73-af751ec4c146_350x300_crop_center.jpg?v=1614690763        ",
+        title:"Here's Why Vitamins are Vital For Skin Health!        ",
+        category:"Anti-inflammatory"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_8ff1a77f-95dc-4c0c-81e8-3d21dd22dce5_350x300_crop_center.jpg?v=1614666005        ",
+        title:" Toners for Oily Skin and Skin-friendly Ingredients        ",
+        category:"Anti-aging"
+
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_4_7dbb5c33-9428-45ac-8a6c-afcbb9bdbb13_350x300_crop_center.jpg?v=1614595568        ",
+        title:"Fuller's Earth: Uses, Benefits and Risks   ",
+        category:"Peptides"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_6_cf3909d9-b7a3-4a8b-915c-50e5b074a23b_350x300_crop_center.jpg?v=1614595081        ",
+        title:" 2 Must have Minimalist serums for winters and how to use these properly        ",
+        category:"Hydration"
+
+    },
+    {
+        url:"https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_5_16fc78c9-d350-40be-8fef-c54a501266f5_350x300_crop_center.jpg?v=1614404374        ",
+        title:"When and how much sunscreen should you apply? What is the best way to use sunscreens?        ",
+        category:"Anti-Oxident"
+
+    },
+    {
+        url:" https://cdn.shopify.com/s/files/1/0410/9608/5665/articles/cover_image_2_338580c7-0238-4c70-a22f-a2c1ac83afae_350x300_crop_center.jpg?v=1614339008        ",
+        title:"  The Importance of Following a Night Skincare Routine        ",
+        category:"Anti-aging"
+
+
+    },
+
+]
+
+let kdataarr = JSON.stringify(dataarr)
+return kdataarr
+}
+
+export default kdata
+
---
+++ scripts/show_two.js
@@ -0,0 +1,39 @@
+
+
+async function append_two(data,location){
+
+    data.map((el,index)=>{
+        let div = document.createElement("div");
+        if(index%2==0){
+            div.setAttribute("class"," knowcardeven");
+        }else{
+            div.setAttribute("class","knowcardodd");
+        }
+        div.setAttribute('data-aos','fade-up');
+
+        let arr = ["Jan","Feb","March","April","May","June","July","August","Sept","Oct","Nov","Dec"]
+        
+        let img = document.createElement("img");
+        img.src = el.url;
+        let divtwo = document.createElement("div");
+        let date = document.createElement("p");
+        date.textContent =arr[Math.floor(Math.random()*12)]+" " + Math.floor(Math.random()*28)+" " + 2021 
+        let h2 = document.createElement("h2");
+        h2.textContent = el.title;
+        let p = document.createElement("button");
+        p.textContent = "Continue reading"
+        p.addEventListener("click",()=>{
+
+            localStorage.setItem("single_Data",JSON.stringify(el));
+            window.location.href = "article.html"
+
+        })
+        divtwo.append(date,h2,p)
+        div.append(img,divtwo);
+        location.append(div);
+    })
+
+
+}
+
+export default append_two

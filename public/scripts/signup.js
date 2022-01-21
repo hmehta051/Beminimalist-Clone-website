d40929039d5d25285810b6dea515d5f67d4ff974
Upasana Kuar <93376720+Upasana1011@users.noreply.github.com>
2022-01-21 15:18:42 +0530

    Add files via upload

---
+++ public/scripts/signup.js
@@ -0,0 +1,8 @@
+function getData(obj){
+    var obj=JSON.parse(obj);
+    console.log(obj)
+if(obj.value=="wrong"){ 
+  var popup= document.getElementById("popup"); 
+  popup.style.display="inline-block"; 
+ }            
+ }

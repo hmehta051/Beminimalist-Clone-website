d76b7201a8bf79270130de5f15849eae061d687d
Sudhanshu894 <sidharts894@gmail.com>
2022-01-23 01:21:46 +0530

    security changes

--- backend/src/configs/database.js
+++ backend/src/configs/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 module.exports = ()=>{
-    return mongoose.connect(`mongodb+srv://hmehta051:Hmehta6_@cluster0.q3a58.mongodb.net/backendprojectunit4`);
+    return mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.q3a58.mongodb.net/backendprojectunit4`);
 } 
